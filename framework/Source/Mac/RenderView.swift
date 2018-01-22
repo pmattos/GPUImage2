98f04f41ca20f573871dfc4aacfd5e1947ecd606
Paulo Mattos <paulo.rr.mattos@icloud.com>
2018-01-22 13:22:15 -0200

    Exports RenderView to Objective-C and bug fixing.
    Bugs fixed in RenderView class:
    * silly typo in `sizeInPixels.height` property
    * caches `self.bounds` so it's accessible outside the main thread

--- framework/Source/Mac/RenderView.swift
+++ framework/Source/Mac/RenderView.swift
@@ -1,9 +1,9 @@
 import Cocoa
 
-public class RenderView:NSOpenGLView, ImageConsumer {
+@objc open class RenderView:NSOpenGLView, ImageConsumer {
     public var backgroundColor = Color.black
     public var fillMode = FillMode.preserveAspectRatio
-    public var sizeInPixels:Size { get { return Size(width:Float(self.frame.size.width), height:Float(self.frame.size.width)) } }
+    public var sizeInPixels:Size { get { return Size(width:Float(self.frame.size.width), height:Float(self.frame.size.height)) } }
 
     public let sources = SourceContainer()
     public let maximumInputs:UInt = 1
@@ -19,7 +19,7 @@ public class RenderView:NSOpenGLView, ImageConsumer {
         glBindFramebuffer(GLenum(GL_FRAMEBUFFER), 0)
         glBindRenderbuffer(GLenum(GL_RENDERBUFFER), 0)
 
-        let viewSize = GLSize(width:GLint(round(self.bounds.size.width)), height:GLint(round(self.bounds.size.height)))
+        let viewSize = GLSize(width:GLint(round(cachedBounds.size.width)), height:GLint(round(cachedBounds.size.height)))
         glViewport(0, 0, viewSize.width, viewSize.height)
 
         clearFramebufferWithColor(backgroundColor)
@@ -30,5 +30,21 @@ public class RenderView:NSOpenGLView, ImageConsumer {
         sharedImageProcessingContext.presentBufferForDisplay()
         
         framebuffer.unlock()
+    }
+    
+    /// Cached view bounds.
+    /// The `NSView.bounds` property can *only* be accessed by the main thread.
+    private var cachedBounds: NSRect = NSRect.zero
+    
+    /// Sets the size of the view’s frame rectangle to the specified dimensions.
+    open override func setFrameSize(_ newSize: NSSize) {
+        super.setFrameSize(newSize)
+        initRenderView()
+        self.cachedBounds = bounds
+    }
+    
+    private func initRenderView() {
+        precondition(Thread.isMainThread)
+        let _ = displayShader
     }
 }
