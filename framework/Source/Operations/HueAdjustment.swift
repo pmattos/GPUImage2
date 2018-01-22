f4df1c7077cbb016fef5a6274f18957a47f89669
Paulo Mattos <paulo.rr.mattos@icloud.com>
2018-01-22 13:24:42 -0200

    Bug fixing in the HueAdjustment filter.
    The shader expected uniform in *radians*, so the filter now converts
    from degrees to radians before setting the corresponding uniform settings.

--- framework/Source/Operations/HueAdjustment.swift
+++ framework/Source/Operations/HueAdjustment.swift
@@ -1,5 +1,10 @@
 public class HueAdjustment: BasicOperation {
-    public var hue:Float = 90.0 { didSet { uniformSettings["hueAdjust"] = hue } }
+    public var hue:Float = 90.0 {
+        didSet {
+            // Shader expects uniform in *radians*.
+            uniformSettings["hueAdjust"] = hue * Float.pi / 180
+        }
+    }
     
     public init() {
         super.init(fragmentShader:HueFragmentShader, numberOfInputs:1)
